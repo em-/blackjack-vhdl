3cff9dd6f3fd0bc342c20c0ad8390329b8e638b2
Emanuele Aina <em@nerd.ocracy.org>
2007-02-09 17:08:22 +0100

    Fixed the digits multiplexation in the display_controller

--- display_controller/display_controller.vhdl
+++ display_controller/display_controller.vhdl
@@ -36,11 +36,12 @@ architecture structural of display_controller is
     signal SEL:   std_logic_vector(1 downto 0);
 begin
 
-STARTED <= SHIFT_OUT(0) or SHIFT_OUT(1) or SHIFT_OUT(2) or SHIFT_OUT(3);
-SEL(0)  <= SHIFT_OUT(2) or SHIFT_OUT(3);
-SEL(1)  <= SHIFT_OUT(1) or SHIFT_OUT(3);
+STARTED  <= SHIFT_OUT(0) or SHIFT_OUT(1) or SHIFT_OUT(2) or SHIFT_OUT(3);
+SHIFT_IN <= not STARTED or SHIFT_OUT(3);
 
-shift_input: mux21_1bit port map ('1', SHIFT_OUT(3), STARTED, SHIFT_IN);
+SEL(0)  <= SHIFT_OUT(1) or SHIFT_OUT(3);
+SEL(1)  <= SHIFT_OUT(2) or SHIFT_OUT(3);
+
 reg: shift_reg generic map (4) port map (CLK, RST, '0', SHIFT_IN, SHIFT_OUT);
 
 mux: for i in 0 to 7 generate
